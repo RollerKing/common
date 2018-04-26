4c27e2b6543ad248a893541434ea3804aa50d00c
JasonQu <qjpcpu@gmail.com>
2018-04-26 22:21:27 +0800

    idemopotent wait

--- redo/work.go
+++ redo/work.go
@@ -5,15 +5,26 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
+	"sync"
 	"syscall"
 	"time"
 )
 
 type Job func(*RedoCtx)
 
+type JobState int
+
+const (
+	JobRunning JobState = iota
+	JobStopping
+	JobStopped
+)
+
 type Recipet struct {
 	done     chan struct{}
 	pls_exit chan struct{}
+	state    JobState
+	*sync.Mutex
 }
 
 type RedoCtx struct {
@@ -32,13 +43,38 @@ func (ctx *RedoCtx) StartNextRightNow() {
 	ctx.SetDelayBeforeNext(time.Duration(0))
 }
 
-func (m *Recipet) Stop() {
-	m.pls_exit <- struct{}{}
+func (m *Recipet) Stop() bool {
+	return m.stopWithRequest(true)
+}
+
+func (m *Recipet) stopWithRequest(with bool) bool {
+	var op bool = false
+	if m.state != JobRunning {
+		return op
+	}
+	m.Lock()
+	if m.state == JobRunning {
+		m.state = JobStopping
+		if with {
+			m.pls_exit <- struct{}{}
+		}
+		op = true
+	}
+	m.Unlock()
+	return op
 }
 
 func (m *Recipet) Wait() {
 	<-m.done
-	close(m.done)
+	if m.state == JobStopped {
+		return
+	}
+	m.Lock()
+	if m.state == JobStopping {
+		m.state = JobStopped
+		close(m.done)
+	}
+	m.Unlock()
 }
 
 func WrapFunc(work func()) Job {
@@ -61,6 +97,8 @@ func Perform(once Job, duration time.Duration) *Recipet {
 	recipet := &Recipet{
 		pls_exit: make(chan struct{}, 1),
 		done:     make(chan struct{}, 1),
+		state:    JobRunning,
+		Mutex:    new(sync.Mutex),
 	}
 	go func(m *Recipet) {
 		sigchan := make(chan os.Signal, 1)
@@ -78,6 +116,8 @@ func Perform(once Job, duration time.Duration) *Recipet {
 			case s := <-sigchan:
 				log.Debugf("get syscall signal %v", s)
 				signal.Stop(sigchan)
+				m.stopWithRequest(false)
+				close(m.pls_exit)
 				m.done <- struct{}{}
 				return
 			case <-time.After(ctx.delayBeforeNextLoop):
