1ec26ce6b8bff684be870e2b48c9115900cda86a
JasonQu <qjpcpu@gmail.com>
2019-01-23 11:21:04 +0800

    test

--- json/json_test.go
+++ json/json_test.go
@@ -1,7 +1,6 @@
 package json
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -10,23 +9,40 @@ func TestDT(t *testing.T) {
 		"a", "b", "a.c.d", "a.f.g",
 	}
 	dtree := createDelTree(paths)
+	var nodes []string
 	vf := func(n *delNode) bool {
-		fmt.Println(n.val)
+		if n.val != "" {
+			nodes = append(nodes, n.val)
+		}
 		return false
 	}
 	walkDelTreeBFS([]*delNode{dtree}, vf)
+	t.Log(nodes)
 }
 
 func TestDel(t *testing.T) {
 	js := `{"name":{"2m":3,"first":"Janet","arr":[1,2],"last":"Prichard","xx":{"yy":324,"tt":3}},"age":47,"weapon":"m4","tx":["s"]}`
-	fmt.Println(js)
 	dels := []string{"name.first", "weapon", "name.xx.yy", "name.2m"}
 	res := TrimJSON(js, dels...)
-	fmt.Println(res)
+	if res != `{"name":{"arr":[1,2],"last":"Prichard","xx":{"tt":3}},"age":47,"tx":["s"]}` {
+		t.Fatal(res)
+	}
 	js = ""
-	fmt.Println(TrimJSON(js, dels...))
-	fmt.Println(TrimJSON(js, ""))
+	res = TrimJSON(js, dels...)
+	if res != "{}" {
+		t.Fatal(res)
+	}
+	res = TrimJSON(js, "")
+	if res != "{}" {
+		t.Fatal(res)
+	}
 	js = `{"name":{"last":"Prichard","xx":{"yy":324,"tt":3}},"age":47,"100":"100",100:1}`
-	fmt.Println(TrimJSON(js, "100", "name.last", "name.xx"))
-	fmt.Println(TrimJSON(js, "100", "name.last", "name.xx", "age"))
+	res = TrimJSON(js, "100", "name.last", "name.xx")
+	if res != `{"age":47}` {
+		t.Fatal(res)
+	}
+	res = TrimJSON(js, "100", "name.last", "name.xx", "age")
+	if res != `{}` {
+		t.Fatal(res)
+	}
 }
