ca65c635932712a0c89b9b8f7b485bdd2eada5c0
JasonQu <qjpcpu@gmail.com>
2019-01-23 13:18:10 +0800

    up

--- utils/collection.go
+++ utils/collection.go
@@ -3,8 +3,151 @@ package utils
 import (
 	"fmt"
 	"reflect"
+	"sort"
 	"strings"
 )
+
+// 求交集
+func InteractStrings(lists ...[]string) []string {
+	for i := range lists {
+		if len(lists[i]) == 0 {
+			return nil
+		}
+		sort.Strings(lists[i])
+	}
+	return interactList(lists, 0, len(lists)-1)
+}
+
+func interactStrings(list1 []string, list2 []string) []string {
+	if len(list1) == 0 || len(list2) == 0 {
+		return nil
+	}
+	var res []string
+	for i, j := 0, 0; i < len(list1) && j < len(list2); {
+		if list1[i] == list2[j] {
+			res = append(res, list1[i])
+			i++
+			j++
+		} else if list1[i] > list2[j] {
+			j++
+		} else {
+			i++
+		}
+	}
+	return res
+}
+
+func interactList(lists [][]string, start, end int) []string {
+	if start > end {
+		return nil
+	}
+	mid := (start + end) / 2
+	var left, right []string
+	switch mid - start {
+	case 0:
+		left = lists[start]
+	case 1:
+		left = interactStrings(lists[start], lists[mid])
+	default:
+		left = interactList(lists, start, mid)
+	}
+	mid++
+	if mid <= end {
+		switch end - mid {
+		case 0:
+			right = lists[mid]
+		case 1:
+			right = interactStrings(lists[mid], lists[end])
+		default:
+			right = interactList(lists, mid, end)
+		}
+	}
+	return interactStrings(left, right)
+}
+
+// 求差集
+func SubstractStrings(list1 []string, list2 []string) []string {
+	sort.Strings(list1)
+	sort.Strings(list2)
+	var res []string
+	var i, j int
+	for i < len(list1) && j < len(list2) {
+		if list1[i] < list2[j] {
+			res = append(res, list1[i])
+			i++
+		} else if list1[i] == list2[j] {
+			i++
+			j++
+		} else {
+			j++
+		}
+	}
+	if i < len(list1) {
+		res = append(res, list1[i:]...)
+	}
+	return res
+}
+
+// 求并集
+func MergeStrings(lists ...[]string) []string {
+	for i := range lists {
+		sort.Strings(lists[i])
+	}
+	return andList(lists, 0, len(lists)-1)
+}
+
+func andList(lists [][]string, start, end int) []string {
+	if start > end {
+		return nil
+	}
+	mid := (start + end) / 2
+	var left, right []string
+	switch mid - start {
+	case 0:
+		left = lists[start]
+	case 1:
+		left = andStrings(lists[start], lists[mid])
+	default:
+		left = andList(lists, start, mid)
+	}
+	mid++
+	if mid <= end {
+		switch end - mid {
+		case 0:
+			right = lists[mid]
+		case 1:
+			right = andStrings(lists[mid], lists[end])
+		default:
+			right = andList(lists, mid, end)
+		}
+	}
+	return andStrings(left, right)
+}
+
+func andStrings(list1 []string, list2 []string) []string {
+	var res []string
+	var i, j int
+	for i < len(list1) && j < len(list2) {
+		if list1[i] == list2[j] {
+			res = append(res, list1[i])
+			i++
+			j++
+		} else if list1[i] > list2[j] {
+			res = append(res, list2[j])
+			j++
+		} else {
+			res = append(res, list1[i])
+			i++
+		}
+	}
+	if i < len(list1) {
+		res = append(res, list1[i:len(list1)]...)
+	}
+	if j < len(list2) {
+		res = append(res, list2[j:len(list2)]...)
+	}
+	return res
+}
 
 func UniqStrings(list []string) []string {
 	memo := make(map[string]int)
---
+++ utils/utils_test.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestAndStrings(t *testing.T) {
+	arr := []string{"a", "b"}
+	brr := []string{"e", "b"}
+	crr := []string{"g"}
+	res := MergeStrings(arr, brr, crr)
+	if strings.Join(res, ".") != "a.b.e.g" {
+		t.Fatal(res)
+	}
+	res = MergeStrings([]string{})
+	if len(res) != 0 {
+		t.Fatal("error")
+	}
+
+	res = SubstractStrings([]string{"a", "b", "c"}, []string{"b", "g"})
+	if strings.Join(res, ".") != "a.c" {
+		t.Fatal(res)
+	}
+	drr := []string{"b", "e"}
+	if len(InteractStrings(arr, brr, crr, drr)) != 0 {
+		t.Fatal("err")
+	}
+	res = InteractStrings(arr, brr, drr)
+	if strings.Join(res, ".") != "b" {
+		t.Fatal(res)
+	}
+}
