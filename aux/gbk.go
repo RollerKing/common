45c2bc2bfd3ee054ea4aabdee99a9d940ad068a0
JasonQu <qjpcpu@gmail.com>
2019-03-12 17:35:00 +0800

    gbk to utf8

---
+++ aux/gbk.go
@@ -0,0 +1,18 @@
+package aux
+
+import (
+	"bytes"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+)
+
+// GbkToUtf8 gbk to utf8
+func GbkToUtf8(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
