d106acd00be477606ab33072767246d338ee0846
JasonQu <qjpcpu@gmail.com>
2020-02-02 16:01:31 +0800

    more should function

--- debug/errors.go
+++ debug/errors.go
@@ -16,6 +16,20 @@ func ShouldBeNil(err error) {
 	}
 }
 
+// ShouldBeTrue would panic if codition is false
+func ShouldBeTrue(condition bool) {
+	if !condition {
+		panic("should be true")
+	}
+}
+
+// ShouldEqual would panic if not equal
+func ShouldEqual(a, b interface{}) {
+	if !reflect.DeepEqual(a, b) {
+		panic(fmt.Sprintf("%v != %v", a, b))
+	}
+}
+
 // AllowPanic swallow panic
 func AllowPanic(fn func()) (isPanicOccur bool) {
 	defer func() {
